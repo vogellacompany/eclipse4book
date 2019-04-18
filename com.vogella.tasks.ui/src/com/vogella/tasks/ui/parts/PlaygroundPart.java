1c5cfc82a442f91b950b76b633b4a5a2aa261a84
Lars Vogel <Lars.Vogel@vogella.com>
2019-04-18 10:52:10 +0200

    Exercises until the CSS part

--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/parts/PlaygroundPart.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/parts/PlaygroundPart.java
@@ -1,14 +1,18 @@
 package com.vogella.tasks.ui.parts;
 
+
 import javax.annotation.PostConstruct;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+
+import com.vogella.swt.widgets.Checkbox;
+
 
 public class PlaygroundPart {
 
 	@PostConstruct
 	public void createControls(Composite parent) {
-	    System.out.println(this.getClass().getSimpleName()
-	    + " @PostConstruct method called.");
+	 Checkbox checkbox = new Checkbox(parent, SWT.NONE);
 	}
 }
--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/parts/TodoDetailsPart.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/parts/TodoDetailsPart.java
@@ -1,5 +1,7 @@
 package com.vogella.tasks.ui.parts;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.di.Focus;
@@ -56,6 +58,51 @@ public class TodoDetailsPart {
 
         btnDone = new Button(parent, SWT.CHECK);
         btnDone.setText("Done");
+
+        updateUserInterface(todo);
+    }
+
+    @Inject
+    public void setTodos(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) List<Todo> todos) {
+        if(todos == null || todos.isEmpty()) {
+            this.todo = java.util.Optional.empty();
+        } else {
+            this.todo = java.util.Optional.of(todos.get(0));
+        }
+        // Remember the todo as field update the user interface
+        updateUserInterface(this.todo);
+    }
+
+    // allows to disable/ enable the user interface fields
+    // if no todo is set
+    private void enableUserInterface(boolean enabled) {
+        if (txtSummary != null && !txtSummary.isDisposed()) {
+            txtSummary.setEnabled(enabled);
+            txtDescription.setEnabled(enabled);
+            dateTime.setEnabled(enabled);
+            btnDone.setEnabled(enabled);
+        }
+    }
+
+    private void updateUserInterface(java.util.Optional<Todo> todo) {
+        // check if Todo is present
+        if (!todo.isPresent()) {
+            enableUserInterface(false);
+            return; // nothing left to do
+        }
+
+        enableUserInterface(true);
+        // the following check ensures that the user interface is available,
+        // it assumes that you have a text widget called "txtSummary"
+        if (txtSummary != null && !txtSummary.isDisposed()) {
+            enableUserInterface(true);
+            txtSummary.setText(todo.get().getSummary());
+            txtDescription.setText(todo.get().getDescription());
+            // more code to fill the widgets with data from your Todo object
+            // more code
+            // ....
+            // ....
+        }
     }
 
     @Focus
